ccbe71ea77b290d60c81554edbb2b77ca7dc4d6f
fryckbos <devnull@localhost>
2009-09-03 15:20:21 +0200

    Updated the manage extension with status checking and more verbose output.

--- workflowengine/manage/WorkflowEngineManage.py
+++ workflowengine/manage/WorkflowEngineManage.py
@@ -1,5 +1,5 @@
 from pymonkey import q
-import signal
+import signal, time
 
 class WorkflowEngineManage:
 
@@ -8,29 +8,50 @@ class WorkflowEngineManage:
         Start workflow engine
         @param pidFile: file path to save the pid
         """
-        pid = q.system.process.runDaemon('%s %s'%(q.system.fs.joinPaths(q.dirs.baseDir, 'bin', 'stackless'), q.system.fs.joinPaths(q.dirs.appDir,'workflowengine','bin', 'main.py')), stdout=q.system.fs.joinPaths(q.dirs.varDir, 'log', 'workflow.stdout'),  stderr=q.system.fs.joinPaths(q.dirs.varDir, 'log', 'workflow.stderr'))
-        pidDir = q.system.fs.getDirName(pidFile)
-        if not q.system.fs.exists(pidDir):
-            q.system.fs.createDir(pidDir)
-        q.system.fs.writeFile(pidFile, str(pid))
+        if self.getStatus() == q.enumerators.AppStatusType.RUNNING:
+            print "The workflowengine is already running."
+        else:
+            print "Starting the workflowengine."
+            pid = q.system.process.runDaemon('%s %s'%(q.system.fs.joinPaths(q.dirs.baseDir, 'bin', 'stackless'), q.system.fs.joinPaths(q.dirs.appDir,'workflowengine','bin', 'main.py')), stdout=q.system.fs.joinPaths(q.dirs.varDir, 'log', 'workflow.stdout'),  stderr=q.system.fs.joinPaths(q.dirs.varDir, 'log', 'workflow.stderr'))
+            pidDir = q.system.fs.getDirName(pidFile)
+            if not q.system.fs.exists(pidDir):
+                q.system.fs.createDir(pidDir)
+            q.system.fs.writeFile(pidFile, str(pid))
 
     def stop(self, pidFile=q.system.fs.joinPaths(q.dirs.pidDir, 'workflowengine', 'workflowengine.pid')):
         """
         Stop workflow engine
         @param pidFile: file path to retrieve the pid
         """
-        pid = self._getPid(pidFile)
-        if self.getStatus(pid=pid) == q.enumerators.AppStatusType.RUNNING:
+        if self.getStatus() == q.enumerators.AppStatusType.RUNNING:
+            print "Stopping the workflowengine."
+            pid = self._getPid(pidFile)
             q.system.process.kill(int(pid), sig=signal.SIGTERM)
+            
+            i = 10
+            while self.getStatus() == q.enumerators.AppStatusType.RUNNING:
+                if i > 0:
+                    print "   Still running, waiting %i seconds..." % i
+                    time.sleep(1)
+                else:
+                    print "Could not stop the workflowengine. Next step: kill it !"
+                    return
+                
+            print "Stopped the workflowengine."
+        else:
+            print "The workflowengine is not running."
 
     def kill(self, pidFile=q.system.fs.joinPaths(q.dirs.pidDir, 'workflowengine', 'workflowengine.pid')):
         """
         Stop workflow engine
         @param pidFile: file path to retrieve the pid
         """
-        pid = self._getPid(pidFile)
-        if self.getStatus(pid=pid) == q.enumerators.AppStatusType.RUNNING:
+        if self.getStatus() == q.enumerators.AppStatusType.RUNNING:
+            print "Killing the workflowengine."
+            pid = self._getPid(pidFile)
             q.system.process.kill(int(pid))
+        else:
+            print "The workflowengine is not running."
 
     def _getPid(self, pidFile = q.system.fs.joinPaths(q.dirs.pidDir, 'workflowengine', 'workflowengine.pid')):
         """
@@ -43,17 +64,14 @@ class WorkflowEngineManage:
 
         return pid
 
-    def getStatus(self,  pidFile = q.system.fs.joinPaths(q.dirs.pidDir, 'workflowengine', 'workflowengine.pid'), pid=None):
+    def getStatus(self,  pidFile = q.system.fs.joinPaths(q.dirs.pidDir, 'workflowengine', 'workflowengine.pid')):
         """
         Retrieve the pid from given file
         @param pidFile: file path to retrieve the pid
         @param pid: pid to check if running
         """
-        if not pid:
-            pid = self._getPid(pidFile)
+        pid = self._getPid(pidFile)
         if pid and q.system.process.isPidAlive(int(pid)):
             return q.enumerators.AppStatusType.RUNNING
         return q.enumerators.AppStatusType.HALTED
-
-
 
