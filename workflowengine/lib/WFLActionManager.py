9d9d5c50e65a237f978a95d8cf7a2cceac159afb
nsami <devnull@localhost>
2009-09-08 10:46:55 +0200

    create tasklets dir if not existing

--- workflowengine/lib/WFLActionManager.py
+++ workflowengine/lib/WFLActionManager.py
@@ -7,7 +7,7 @@ from workflowengine.Exceptions import ActionNotFoundException, WFLException
 from concurrence import Tasklet, Message
 
 ActionManagerTaskletPath = q.system.fs.joinPaths(q.dirs.appDir,'workflowengine','tasklets')
-ActorActionTaskletPath = q.system.fs.joinPaths(ActionManagerTaskletPath, 'actor') 
+ActorActionTaskletPath = q.system.fs.joinPaths(ActionManagerTaskletPath, 'actor')
 RootobjectActionTaskletPath = q.system.fs.joinPaths(ActionManagerTaskletPath, 'rootobject')
 
 class MSG_ACTION_CALL(Message): pass
@@ -19,55 +19,60 @@ class WFLActionManager():
     """
     This class is hosted on q.workflowengine.actionmanager.
     Will result in a workflow being executed which implements the action: the worklflows are tasklet scripts.
-    
+
     Location of root object actions:
     ##/opt/qbase3/apps/workflowengine/tasklets/rootobject/$actorname/$actionname/
-    
+
     Location of actor actions:
     ##/opt/qbase3/apps/workflowengine/tasklets/actor/$actorname/$actionname/
     """
-    
+
     def __init__(self):
         self.__taskletEngine = TaskletsEngine()
+        ##create tasklets dir if it doesnt exist
+        if not q.system.fs.exists(ActionManagerTaskletPath):
+            q.system.fs.createDir(ActionManagerTaskletPath)
         self.__taskletEngine.addFromPath(ActorActionTaskletPath)
+        if not q.system.fs.exists(RootobjectActionTaskletPath):
+            q.system.fs.createDir(RootobjectActionTaskletPath)
         self.__taskletEngine.addFromPath(RootobjectActionTaskletPath)
-    
+
     def init(self):
         '''
         Does nothing: used to force pymonkey to create the q-tree mapping and initialize the taskletengine.
         '''
         pass
-    
+
     def startActorAction(self, actorname, actionname, params, executionparams={}, jobguid=None):
         """
         Starts a given Actor Action. Uses the tasklet engine to run the matching tasklets.
-        
-        The current job will be determined automatically, it can however be overriden by setting jobguid.      
+
+        The current job will be determined automatically, it can however be overriden by setting jobguid.
         The action will be executed in a new job that is the newest child of the current job, the new job will inherit the following properties of the job: name, description, userErrormsg, internalErrormsg and maxduration.
-        
+
         The executionparams passed to the function will override the properties of the job (if provided).
 
         The guid of the job in which the action is executed, will be added to params: params['jobguid'].
         The tasklets executing in the Actor Action have to add their result in params['result']. The dictionary returned by startActorAction contains this result, under the 'result' key.
-        
+
         @param actorname:                      Name of the actor of the actor action.
         @type actorname:                       string
-        
+
         @param actionnmame:                    Name of the action of the actor action.
         @type actionname:                      string
-        
-        @param params:                         Dictionary containing all parameters required to execute this actions. 
+
+        @param params:                         Dictionary containing all parameters required to execute this actions.
         @type params:                          dictionary
-        
-        @param executionParams:                Dictionary can following keys: name, description, userErrormsg, internalErrormsg, maxduration, wait, timetostart, priority 
+
+        @param executionParams:                Dictionary can following keys: name, description, userErrormsg, internalErrormsg, maxduration, wait, timetostart, priority
         @type executionParams:                 dictionary
-        
+
         @param jobguid:                        Optional parameter, can be used to override the current job.
         @type jobguid:                         guid
 
         @return:                               Dictionary with action result as result and the action's jobguid: {'result': <result>, 'jobguid': guid}
         @rtype:                                dictionary
-        
+
         @raise ActionNotFoundException:        In case no actions are found for the specified actorname and actoraction
         @raise e:                              In case an error occurred, exception is raised
         """
@@ -75,7 +80,7 @@ class WFLActionManager():
             raise ActionNotFoundException("ActorAction", actorname, actionname)
         #SETUP THE JOB AND THE PARAMS
         currentjob = (jobguid and q.drp.job.get(jobguid)) or Tasklet.current().job.job
-        job = WFLJob(parentjob=currentjob, actionName=actorname+"."+actionname, executionparams=executionparams) 
+        job = WFLJob(parentjob=currentjob, actionName=actorname+"."+actionname, executionparams=executionparams)
         params['jobguid'] = job.getJobGUID()
         #START A NEW TASKLET FOR THE JOB
         tasklet = Tasklet.new(self.__execute)(Tasklet.current(), job, params, (actorname, actionname), ActorActionTaskletPath)
@@ -87,37 +92,37 @@ class WFLActionManager():
             return { 'jobguid':job.getJobGUID(), 'result':params.get('result')}
         elif msg.match(MSG_ACTION_EXCEPTION):
             raise args[0]
-        
+
     def startRootobjectAction(self, rootobjectname, actionname, params, executionparams={}, jobguid=None):
-        """        
+        """
         Starts a given RootObject Action. Uses the tasklet engine to run the matching tasklets.
-        
-        The current job will be determined automatically, it can however be overriden by setting jobguid.      
+
+        The current job will be determined automatically, it can however be overriden by setting jobguid.
         The action will be executed in a new job that is the newest child of the current job, the new job will inherit the following properties of the job: name, description, userErrormsg, internalErrormsg and maxduration.
-        
+
         The executionparams passed to the function will override the properties of the job (if provided).
         .
         The guid of the job in which the action is executed, will be added to params: params['jobguid'].
         The tasklets executing in the Actor Action have to add their result in params['result']. The dictionary returned by startActorAction contains this result, under the 'result' key.
-        
+
         @param rootobjectname:                 Name of the rootobject of the root object action.
         @type type:                            string
-        
+
         @param actionnmame:                    Name of the action to execute on the rootobject.
         @type actionname:                      string
-        
-        @param params:                         Dictionary containing all parameters required to execute this actions. 
+
+        @param params:                         Dictionary containing all parameters required to execute this actions.
         @type params:                          dictionary
-        
-        @param executionParams:                Dictionary can following keys: name, description, userErrormsg, internalErrormsg, maxduration, wait, timetostart, priority, clouduserguid, rootobjecttype, rootobjectguid 
+
+        @param executionParams:                Dictionary can following keys: name, description, userErrormsg, internalErrormsg, maxduration, wait, timetostart, priority, clouduserguid, rootobjecttype, rootobjectguid
         @type executionParams:                 dictionary
-        
+
         @param jobguid:                        Optional parameter, can be used to override the current job.
         @type jobguid:                         guid
-        
+
         @return:                               dictionary with action result as result and the action's jobguid: {'result': <result>, 'jobguid': guid}
         @rtype:                                dictionary
-        
+
         @raise ActionNotFoundException:        In case no actions are found for the specified rootobjectname and actoraction
         @raise e:                              In case an error occurred, exception is raised
         """
@@ -143,9 +148,9 @@ class WFLActionManager():
         #SETUP THE CONTEXT
         Tasklet.current().job = job
         Tasklet.current().tags = tags
-        
+
         if job.wait is False: MSG_ACTION_NOWAIT.send(parentTasklet)()
-        
+
         #EXECUTE THE TASKLETS
         try:
             self.__taskletEngine.execute(params, tags=tags, path=path)
