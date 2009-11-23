579078842ef134cf2ae408ef67e4e2cca36ca8ac
nsami <devnull@localhost>
2009-11-23 17:55:22 +0100

    STENGLOC-1953 modify workflowengine to import DrpClient (mapped on q.drp)

--- workflowengine/manage/wfe_extension.py
+++ workflowengine/manage/wfe_extension.py
@@ -14,29 +14,27 @@ if stackless:
     from workflowengine.WFLActionManager import WFLActionManager
     from workflowengine.WFLAgentController import WFLAgentController
     from workflowengine.WFLJobManager import WFLJobManager
-    from workflowengine.DRPClient import DRPClient
-    
+
     ActionManager = WFLActionManager
     AgentController = WFLAgentController
     JobManager = WFLJobManager
-    Drp = DRPClient
-    
+
 else:
-    
+
     class Dummy:
         pass
-    
+
     if inAppserver():
         from workflowengine.CloudAPIActionManager import WFLActionManager
         ActionManager = WFLActionManager
     else:
         ActionManager = Dummy
-    
+
     JobManager = Dummy
     AgentController = Dummy
-    
-    from workflowengine.QshellDRPClient import DRPClient
-    Drp = DRPClient
+
+from workflowengine.QshellDRPClient import DRPClient
+Drp = DRPClient
 
 
 class WFLConfigItem(ConfigManagementItem):
@@ -50,7 +48,7 @@ class WFLConfigItem(ConfigManagementItem):
     def ask(self):
         self.dialogAskInteger('port', 'The port of workflowengine socket', 9876)
         # osis_address: doesn't work with localhost in stead of 127.0.0.1. Due to ConcurrenceSocket limitations.
-        self.dialogAskString('osis_address', 'The address of the applicationserver running the OSIS service', 'http://127.0.0.1:8888') 
+        self.dialogAskString('osis_address', 'The address of the applicationserver running the OSIS service', 'http://127.0.0.1:8888')
         self.dialogAskString('osis_service', 'The name of the OSIS service', 'osis_service')
         self.dialogAskString('xmppserver', 'The DNS address of the XMPP server', None)
         self.dialogAskString('agentcontrollerguid', 'The agentguid of the agentcontroller', None)
