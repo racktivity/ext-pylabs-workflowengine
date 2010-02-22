9b609208d607aa55e692d58ebf6abda2b8f3cfac
fryckbos <devnull@localhost>
2010-02-22 19:47:53 +0100

    An example appserver service for the QueueInterface, uses twisted callbacks.

--- workflowengine/tests/apps/applicationserver/services/WFLTestService/testservice.py
+++ workflowengine/tests/apps/applicationserver/services/WFLTestService/testservice.py
@@ -1,14 +1,17 @@
 from pymonkey import q, i
 
-
 class WFLTestService:
     def __init__(self):
         pass
     
     @q.manage.applicationserver.expose
+    @q.manage.applicationserver.not_threaded
     def test(self, num):
         ret = q.workflowengine.actionmanager.startRootobjectAction("ro_test", "test", {'num':num}, executionparams={'maxduration':10})
-                
-        params = ret['result']
-        jobguid = ret['jobguid']
-        return "Job " + jobguid + ": test successful : " + str(params)
+        def generateRetValue(ret):	
+                params = ret['result']
+                jobguid = ret['jobguid']
+                return "Job " + jobguid + ": test successful : " + str(params)
+        ret.addCallback(generateRetValue)
+	return ret
+
