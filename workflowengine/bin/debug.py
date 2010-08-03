2d7e50e3a677c31d58bd1e2fa07b67007b585058
nsami <devnull@localhost>
2010-08-03 10:30:59 +0200

    modify debug.py

--- workflowengine/bin/debug.py
+++ workflowengine/bin/debug.py
@@ -1,30 +1,32 @@
-from workflowengine.SharedMemory import open_shm, close_shm, write_shm
-import yaml
+from workflowengine.SharedMemory import open_shm, close_shm
+import ast
 
 def main():
-	currentjob = open_shm("wfe-current-job")	
-	currentjob.seek(0)
-        print("Currently running: " + currentjob.readline())
-	close_shm("wfe-current-job", currentjob)
+    currentjob = open_shm("wfe-current-job")
+    currentjob.seek(0)
+    print("Currently running: " + currentjob.readline())
+    close_shm("wfe-current-job", currentjob)
 
-	jobs = open_shm("wfe-jobs")
-	jobs.seek(0)
-	jobs_string = ""
-	line = jobs.readline()
-	while line <> "---\n":
-		jobs_string += line
-		line = jobs.readline()
-	close_shm("wfe-jobs", jobs)
-	jobs_content = yaml.load(jobs_string)
-	for jobguid in jobs_content:
-		job = jobs_content[jobguid]
-		print "Job " + str(jobguid)
-		print "  actionName " + str(job[0])
-		print "  parentjobguid " + str(job[1])
-		print "  jobstatus " + str(job[2])
-		print "  starttime " + str(job[3])
-		print "  agentguid " + str(job[4])
-		print
+    jobs = open_shm("wfe-jobs")
+    jobs.seek(0)
+    jobs_string = ""
+    line = jobs.readline()
+    while line <> "---\n":
+        jobs_string += line
+        line = jobs.readline()
+
+    close_shm("wfe-jobs", jobs)
+    jobs_content =  ast.literal_eval(jobs_string)
+
+    for jobguid, details in jobs_content.iteritems():
+        print "Job " + str(jobguid)
+        print "  actionName " + str(details['actionname'])
+        print "  parentjobguid " + str(details['parentjobguid'])
+        print "  jobstatus " + str(details['jobstatus'])
+        print "  starttime " + str(details['starttime'])
+        print "  agentguid " + str(details['agentguid'])
+        print
+
 
 if __name__ == '__main__':
-	main()
+    main()
